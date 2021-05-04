9726062996fadea68af0470148bb4f7b506fa7e0
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2021-05-04 20:35:10 +0600

    owl

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".owl-carousel-1").owlCarousel({
+        items:3,
+        margin: 20,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:1000
+    });
+});
+$(document).ready(function(){
+    $(".owl-carousel-2").owlCarousel({
+        items: 3,
+        loop: true,
+        margin:30,
+        dots:true,
+        autoplay: true,
+        autoplayTimeout: 1500,
+    });
+});
